27dbc90db1fa642dce81a00f5d04454a5601810e
raduloov <raduloov@gmail.com>
2022-01-25 13:13:34 +0200

    UI bug fix

--- src/components/ItemsContainer.js
+++ src/components/ItemsContainer.js
@@ -2,7 +2,7 @@ import ItemCard from './UI/ItemCard';
 
 const ItemsContainer = ({ items }) => {
   return (
-    <div className="flex flex-wrap mt-10 sm:justify-center">
+    <div className="flex flex-wrap sm:mt-20 md:mt-10 sm:justify-center">
       {items.map(item => (
         <ItemCard data={item} key={item.id} />
       ))}
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,7 +4,7 @@ const MainContainer = props => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="sm:w-full md:w-[1360px] bg-[rgba(255,255,255,0.1)] shadow-md rounded-b-xl">
-        <div className="flex justify-between items-center mt-7 mb-2">
+        <div className="flex justify-between items-center mt-7">
           <Dropdown chooseCategory={props.onChooseCategory} />
         </div>
         {props.children}
